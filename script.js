1b7cb39d71fa7aeb3ff1c2be29bbf4f8c79f613c
20106050083 <agungrashif009@gmail.com>
2024-03-23 10:35:49 +0700

    Initial app from ch1

---
+++ script.js
@@ -0,0 +1,12 @@
+var slider = tns({
+  container: ".my-slider",
+  items: 2,
+  gutter: 30,
+  slideBy: 1,
+  center: true,
+  mouseDrag: true,
+  autoplay: true,
+  nav: false,
+  autoplayButtonOutput: false,
+  controlsContainer: "#custom-control",
+});
